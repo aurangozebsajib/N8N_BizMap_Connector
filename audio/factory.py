ba7982d54f4896c5f104838ad2d725e2f08c9477
aurangozebsajib <sarkarsajib698@gmail.com>
2026-09-25 09:45:14 +0600

    Update factory.py

--- audio/factory.py
+++ audio/factory.py
@@ -69,7 +69,7 @@ KEYWORDS: 5 keywords comma separated
 """
 
 response = client.models.generate_content(
-        model="gemini-2.5-flash",
+    model="gemini-1.5-flash",
     contents=prompt
 )
 bangla_script = response.text
